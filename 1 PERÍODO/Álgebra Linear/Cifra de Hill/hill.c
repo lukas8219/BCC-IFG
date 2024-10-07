2a833b29bf7ad7aa1735cf8f01d56859fc726491
Karolayne <karolayneamabile@gmail.com>
2024-10-06 22:08:29 -0300

    add hillcipher

--- 1 PERÍODO/Álgebra Linear/Cifra de Hill/hill.c
+++ 1 PERÍODO/Álgebra Linear/Cifra de Hill/hill.c
@@ -3,48 +3,70 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-int makeTextIndex(char text[100]);
-int getIndex(char letter);
-
 const int key[2][2] = {{3, 1}, {5, 2}};
-const int inversedkey[2][2] = {{2, -1}, {-5, 3}};
+const int inverseKey[2][2] = {{2, -1}, {-5, 3}};
+
+char plainText[100];
+char cipherText[100] = "PTRVRILWWMUN4W";
+int length = 0; 
+
+void applyHillCipher(void);
+void applyHillDecryption (void);
 
 int main(void)
 {
-    char text[100] = "bom dia";
-    int *textIndex[100] = makeTextIndex(text);
-    for (int i = 0; i < 100; i++)
-    {
-        printf("%i", text[i]);
-    }
+    applyHillDecryption();
+    printf("%s", plainText);
 }
 
-int makeTextIndex(char text[100])
+void applyHillCipher(void)
 {
-    int TextIndex[100];
-    for (int i = 0; i < strlen(text); i++)
-    {
-        if(isalnum(text[i]))
-        {
-            TextIndex[i] = getIndex(text[i]);
+    int i, j = 0;
+    for (i = 0; i < strlen(plainText); i++) {
+        if (isalnum(plainText[i])) {
+            cipherText[j] = toupper(plainText[i]);
+            j++;
         }
     }
-    return &TextIndex;
+    cipherText[j] = '\0';
+    length = strlen(cipherText);
+
+    char tmp1, tmp2;
+    for (int i = 0; i < length; i += 2)
+    {
+        tmp1 = ((cipherText[i] - 'A') * key[0][0]) + ((cipherText[i + 1] - 'A') * key[0][1]);
+        tmp1 %= 26; 
+
+        tmp2 = ((cipherText[i] - 'A') * key[1][0]) + ((cipherText[i + 1] - 'A') * key[1][1]);
+        tmp2 %= 26; 
+
+        cipherText[i] = tmp1 + 'A';
+        cipherText[i + 1] = tmp2 + 'A';
+    }
 }
 
-int getIndex(char letter)
-{   
-    char alphabet[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int index = -1;
-
-    letter = toupper(letter);
-    for (int i = 0; i < 26; i++)
-    {
-        if (alphabet[i] == letter)
-        {
-            index = i;
-            break;
+void applyHillDecryption (void)
+{
+    int i, j = 0;
+    for (i = 0; i < strlen(cipherText); i++) {
+        if (isalnum(cipherText[i])) {
+            plainText[j] = toupper(cipherText[i]);
+            j++;
         }
     }
-    return index;
+    plainText[j] = '\0';
+    length = strlen(plainText);
+
+    char tmp1, tmp2;
+    for (int i = 0; i < length; i += 2)
+    {
+        tmp1 = (((plainText[i] - 'A') * inverseKey[0][0]) + ((plainText[i + 1] - 'A') * inverseKey[0][1])) + 26;
+        tmp1 %= 26; 
+
+        tmp2 = (((plainText[i] - 'A') * inverseKey[1][0]) + ((plainText[i + 1] - 'A') * inverseKey[1][1])) + 26;
+        tmp2 %= 26; 
+
+        plainText[i] = tmp1 + 'A';
+        plainText[i + 1] = tmp2 + 'A';
+    }
 }
